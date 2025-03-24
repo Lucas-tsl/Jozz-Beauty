ae1be11eb37eb047816c968b5ac53b9c4082a460
TROTESEIL Lucas <113380700+Lucas-tsl@users.noreply.github.com>
2025-03-24 15:57:52 +0100

    Update categoryNameInChild.php
    J'ai mis en paramètre les catégories concernés pour ne pas avoir de problème sur les autres

--- categoryNameInChild.php
+++ categoryNameInChild.php
@@ -1,15 +1,19 @@
 function add_back_to_parent_category() {
     if (is_product_category()) {
         $term = get_queried_object();
-        if ($term && $term->parent) {
+        
+        // Liste des catégories concernées
+        $target_slugs = ['le-teint', 'les-levres', 'les-yeux', 'le-teint-maquillage', 'les-levres-maquillage', 'les-yeux-maquillage', 'les_yeux'];
+        
+        if ($term && $term->parent && in_array($term->slug, $target_slugs)) {
             $parent_category = get_term($term->parent, 'product_cat');
 
             // Forcer un affichage correct des caractères spéciaux
             $parent_name = apply_filters('the_title', $parent_category->name);
 
-            echo '<div class="return-to-parent" style="margin-bottom: 20px;">
-                    <a href="' . esc_url(get_term_link($parent_category)) . '"  class="categ-back"">
-                        < Revenir au ' . esc_html($parent_name) . '
+            echo '<div class="return-to-parent">
+                    <a href="' . esc_url(get_term_link($parent_category)) . '" class="categ-back">
+                        ⬅ Retour à ' . esc_html($parent_name) . '
                     </a>
                   </div>';
         }
